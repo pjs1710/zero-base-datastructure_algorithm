08df9fa415b06464e5c6e4a9e6a843b0fc782b03
Park Jae Seong <timer973@naver.com>
2025-05-09 13:38:18 +0900

    LinearDS_02 : 선형자료구조 - 해시 테이블 - 1

---
+++ src/main/java/dev/timer/lineards_02/hash/Practice2.java
@@ -0,0 +1,51 @@
+package dev.timer.lineards_02.hash;
+
+// 해시 충돌 해결 - 개방 주소법 (선형 탐사법)
+
+class MyHashTable2 extends MyHashTable {
+
+    MyHashTable2(int size) {
+        super(size);
+    }
+
+    public void setValue(int key, int data) {
+        int idx = this.getHash(key);
+
+        if (this.elemCnt == this.table.length) {
+            System.out.println("Hash table is Full!");
+            return;
+        } else if (this.table[idx] == null) {
+            this.table[idx] = data;
+        } else {
+            int newIdx = idx;
+            while (true) {
+                newIdx = (newIdx + 1) % this.table.length;
+                if (this.table[newIdx] == null) {
+                    break;
+                }
+            }
+            this.table[newIdx] = data;
+        }
+        elemCnt++;
+    }
+}
+
+public class Practice2 {
+
+    public static void main(String[] args) {
+        // Test Code
+        MyHashTable2 ht = new MyHashTable2(5);
+
+        ht.setValue(1, 1);
+        ht.setValue(3, 3);
+        ht.printHashTable();
+
+        ht.setValue(1, 10);
+        ht.printHashTable();
+
+        ht.setValue(1, 20);
+        ht.setValue(1, 30);
+        ht.setValue(1, 40);
+        ht.printHashTable();
+    }
+}
