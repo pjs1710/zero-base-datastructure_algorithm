e3303adf94bff0647baff2424619760014e9f6b7
Park Jae Seong <timer973@naver.com>
2025-05-10 00:35:27 +0900

    LinearDS_02 : 선형자료구조 - 연습 문제 풀이 1-2

--- src/main/java/dev/timer/lineards_02/practice/Practice5.java
+++ src/main/java/dev/timer/lineards_02/practice/Practice5.java
@@ -62,6 +62,7 @@ public class Practice5 {
 
     public static void main(String[] args) {
         // Test Code
+        // Case 1
         int n = 6;
         int k = 3;
         int l = 3;
@@ -75,5 +76,40 @@ public class Practice5 {
         moves.add(new ArrayList(Arrays.asList(15, 'L')));
         moves.add(new ArrayList(Arrays.asList(7, 'D')));
         System.out.println((solution(n, k, l, apples, moves)));
+
+        // Case 2
+        n = 10;
+        k = 4;
+        l = 4;
+        apples.clear();
+        apples.add(new ArrayList<>(Arrays.asList(1, 2)));
+        apples.add(new ArrayList<>(Arrays.asList(1, 3)));
+        apples.add(new ArrayList<>(Arrays.asList(1, 4)));
+        apples.add(new ArrayList<>(Arrays.asList(1, 5)));
+
+        moves.clear();
+        moves.add(new ArrayList(Arrays.asList(8, 'D')));
+        moves.add(new ArrayList(Arrays.asList(10, 'D')));
+        moves.add(new ArrayList(Arrays.asList(11, 'D')));
+        moves.add(new ArrayList(Arrays.asList(13, 'L')));
+        System.out.println(solution(n, k, l, apples, moves));
+
+        // Case 3
+        n = 10;
+        k = 5;
+        l = 4;
+        apples.clear();
+        apples.add(new ArrayList<>(Arrays.asList(1, 5)));
+        apples.add(new ArrayList<>(Arrays.asList(1, 3)));
+        apples.add(new ArrayList<>(Arrays.asList(1, 2)));
+        apples.add(new ArrayList<>(Arrays.asList(1, 6)));
+        apples.add(new ArrayList<>(Arrays.asList(1, 7)));
+
+        moves.clear();
+        moves.add(new ArrayList(Arrays.asList(8, 'D')));
+        moves.add(new ArrayList(Arrays.asList(10, 'D')));
+        moves.add(new ArrayList(Arrays.asList(11, 'D')));
+        moves.add(new ArrayList(Arrays.asList(13, 'L')));
+        System.out.println(solution(n, k, l, apples, moves));
     }
 }
