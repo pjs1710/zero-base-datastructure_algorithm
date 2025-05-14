a70cbe73287370313ef9cf277bfba7aaee46e04f
Park Jae Seong <timer973@naver.com>
2025-05-15 01:24:14 +0900

    NonLinearDS_03 : 비선형 자료구조 - 이진 탐색 트리 문제 풀이 1번

---
+++ src/main/java/dev/timer/nonlineards_03/binary_search_tree/problem_solve/Practice1.java
@@ -0,0 +1,96 @@
+package dev.timer.nonlineards_03.binary_search_tree.problem_solve;
+
+// 주어진 이진 탐색 트리에서 N번째로 작은 수 구하기
+// -> 그냥 중위 순회하면 오름차순으로 정렬된 트리의 데이터가 나옴
+
+// 입력 트리 : 3, 1, 4, null, 2
+// N : 1
+// 결과 : 1
+
+// 입력 트리 : 5, 3, 6, 2, 4, null, null, 1
+// N : 3
+// 결과 : 3
+
+import java.util.ArrayList;
+
+class Node {
+    int key;
+    Node left;
+    Node right;
+
+    public Node(int key, Node left, Node right) {
+        this.key = key;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class BinarySearchTree {
+    Node head;
+
+    BinarySearchTree() {
+
+    }
+
+    BinarySearchTree(int key) {
+        this.head = new Node(key, null, null);
+    }
+
+    public void addNode(int key) {
+        this.head = this.addNode(this.head, key);
+    }
+
+    public Node addNode(Node cur, int key) {
+        if (cur == null) {
+            return new Node(key, null, null);
+        }
+
+        if (key < cur.key) {
+            cur.left = addNode(cur.left, key);
+        } else {
+            cur.right = addNode(cur.right, key);
+        }
+
+        return cur;
+    }
+}
+
+public class Practice1 {
+
+    public static void solution(Integer[] data, int n) {
+        BinarySearchTree bst = new BinarySearchTree(data[0]);
+
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] == null) {
+                continue;
+            }
+
+            bst.addNode(data[i]);
+        }
+
+        ArrayList<Integer> list = new ArrayList<>();
+        inOrder(bst.head, list);
+        System.out.println(list.get(n - 1));
+    }
+
+    public static void inOrder(Node node, ArrayList list) {
+        if (node == null) {
+            return;
+        }
+
+        inOrder(node.left, list);
+        list.add(node.key);
+        inOrder(node.right, list);
+    }
+
+    public static void main(String[] args) {
+        // Test Code
+        Integer[] data = {3, 1, 4, null, 2};
+        int n = 1;
+        solution(data, n);
+
+        data = new Integer[]{5, 3, 6, 2, 4, null, null, 1};
+        n = 3;
+        solution(data, n);
+    }
+}
