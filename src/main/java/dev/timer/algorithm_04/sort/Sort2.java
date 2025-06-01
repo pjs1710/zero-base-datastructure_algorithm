ee39125f73a0167a2d68d87709a7351aa2eee76c
Park Jae Seong <timer973@naver.com>
2025-06-01 19:09:48 +0900

    Algorithm_04 : 정렬_2 합병 정렬

---
+++ src/main/java/dev/timer/algorithm_04/sort/Sort2.java
@@ -0,0 +1,47 @@
+package dev.timer.algorithm_04.sort;
+
+import java.util.Arrays;
+
+public class Sort2 {
+
+    public static void mergeSort(int[] arr, int[] tmp, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+            mergeSort(arr, tmp, left, mid);
+            mergeSort(arr, tmp, mid + 1, right);
+            merge(arr, tmp, left, right, mid);
+        }
+    }
+
+    public static void merge(int[] arr, int[] tmp, int left, int right, int mid) {
+        int p = left;
+        int q = mid + 1;
+        int idx = p;
+
+        while (p <= mid || q <= right) {
+            if (p <= mid && q <= right) {
+                if (arr[p] <= arr[q]) {
+                    tmp[idx++] = arr[p++];
+                } else {
+                    tmp[idx++] = arr[q++];
+                }
+            } else if (p <= mid && q > right) {
+                tmp[idx++] = arr[p++];
+            } else {
+                tmp[idx++] = arr[q++];
+            }
+        }
+
+        for (int i = left; i <= right; i++) {
+            arr[i] = tmp[i];
+        }
+    }
+
+    public static void main(String[] args) {
+        // Test Code
+        int[] arr = {3, 5, 2, 7, 1, 4, 6};
+        int[] tmp = new int[arr.length];
+        mergeSort(arr, tmp, 0, arr.length - 1);
+        System.out.println("합병 정렬 : " + Arrays.toString(arr));
+    }
+}
