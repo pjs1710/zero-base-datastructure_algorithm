8da73e87c0cac7f146bd68d91ec177f53b460742
Park Jae Seong <timer973@naver.com>
2025-06-02 07:42:30 +0900

    Algorithm_04 : 정렬_3 계수 정렬

--- src/main/java/dev/timer/algorithm_04/sort/Sort3.java
+++ src/main/java/dev/timer/algorithm_04/sort/Sort3.java
@@ -1,9 +1,6 @@
 package dev.timer.algorithm_04.sort;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class Sort3 {
 
@@ -49,11 +46,50 @@ public class Sort3 {
         return maxLen;
     }
 
+    public static void countingSort(int[] arr) {
+        int max = Arrays.stream(arr).max().getAsInt();
+        int[] cntArr = new int[max + 1];
+
+        for (int i = 0; i < arr.length; i++) {
+            cntArr[arr[i]]++;
+        }
+
+        int idx = 0;
+        for (int i = 0; i < cntArr.length; i++) {
+            while (cntArr[i] > 0) {
+                arr[idx++] = i;
+                cntArr[i] -= 1;
+            }
+        }
+
+        // max 값이 커졌을 때를 대비해서 만든 HashMap 보완 솔루션
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int item : arr) {
+            map.put(item, map.getOrDefault(item, 0) + 1);
+        }
+
+        int idx2 = 0;
+        ArrayList<Integer> list = new ArrayList<>(map.keySet());
+        Collections.sort(list);
+
+        for (int i = 0; i < list.size(); i++) {
+            int cnt = map.get(list.get(i));
+            while (cnt > 0) {
+                arr[idx2++] = list.get(i);
+                cnt--;
+            }
+        }
+    }
+
     public static void main(String[] args) {
 
         // Test Code
         int[] arr = {10, 32, 52, 27, 48, 17, 99, 56};
         radixSort(arr);
         System.out.println("기수 정렬 : " + Arrays.toString(arr));
+
+        // Test Code
+        countingSort(arr);
+        System.out.println("계수 정렬 : " + Arrays.toString(arr));
     }
 }
