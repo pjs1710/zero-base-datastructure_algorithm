ff79ace34c82ab5baf21126b28dbda1c2cf9e09a
Park Jae Seong <timer973@naver.com>
2025-06-20 18:53:47 +0900

    Algorithm : DP 최적화 적용

--- src/main/java/dev/challenge/dp/Main.java
+++ src/main/java/dev/challenge/dp/Main.java
@@ -3,11 +3,16 @@ package dev.challenge.dp;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
 
     static final int INF = 987654321;
+    static final int UND = -1;
     static int cnt = 0;
+    static List<Integer> memo;
 
     public static int f(int x) {
         cnt++;
@@ -15,21 +20,36 @@ public class Main {
             return 0;
         }
 
+        if (memo.get(x) != UND) {
+            return memo.get(x);
+        }
+
         int ret = INF;
 
         if (x % 3 == 0) {
-            ret = Math.min(f(x / 3), ret);
+            if (memo.get(x / 3) != UND) {
+                ret = Math.min(memo.get(x / 3), ret);
+            } else {
+                ret = Math.min(f(x / 3), ret);
+            }
         }
         if (x % 2 == 0) {
-            ret = Math.min(f(x / 2), ret);
+            if (memo.get(x / 2) != UND) {
+                ret = Math.min(memo.get(x / 2), ret);
+            } else {
+                ret = Math.min(f(x / 2), ret);
+            }
         }
+        memo.set(x, Math.min(f(x - 1), ret) + 1);
 
-        return Math.min(f(x - 1), ret) + 1;
+        return memo.get(x);
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int X = Integer.parseInt(br.readLine());
+
+        memo = new ArrayList<>(Collections.nCopies(X + 1, UND));
 
         int result = f(X);
         System.out.println(result);
