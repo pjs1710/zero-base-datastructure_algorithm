73bddb3a419faa8dbab5023a8d98e17d657dd9e8
Park Jae Seong <timer973@naver.com>
2025-06-23 23:20:39 +0900

    Algorithm : 점화식 메모이제이션 적용

--- src/main/java/dev/challenge/algorithm/Main.java
+++ src/main/java/dev/challenge/algorithm/Main.java
@@ -3,24 +3,38 @@ package dev.challenge.algorithm;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
+
+    static final long UND = -1;
+    static List<Long> memo;
 
     public static long t(int n) {
         if (n == 0) {
             return 1;
+        }
+
+        if (memo.get(n) != UND) {
+            return memo.get(n);
         }
         long ret = 0;
 
         for (int i = 0; i < n; i++) {
             ret += t(i) * t(n - i - 1); // i + n - i - 1 = n - 1
         }
+
+        memo.set(n, ret);
         return ret;
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
+
+        memo = new ArrayList<>(Collections.nCopies(N + 1, UND));
         System.out.println(t(N));
     }
 }
