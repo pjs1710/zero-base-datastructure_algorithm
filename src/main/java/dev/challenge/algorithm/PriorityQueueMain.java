16bef19f529aefcea5b3981e05e0e690913a3e58
Park Jae Seong <timer973@naver.com>
2025-06-26 00:56:59 +0900

    Algorithm : 우선순위 큐

---
+++ src/main/java/dev/challenge/algorithm/PriorityQueueMain.java
@@ -0,0 +1,47 @@
+package dev.challenge.algorithm;
+
+import java.util.PriorityQueue;
+
+class Point implements Comparable<Point> {
+    int x, y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public int compareTo(Point other) {
+        if (this.x == other.x) {
+            return this.y - other.y;
+        }
+
+        return this.x - other.x;
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
+
+public class PriorityQueueMain {
+
+    public static void main(String[] args) {
+        PriorityQueue<Point> pq = new PriorityQueue<>();
+        pq.add(new Point(4, 5));
+        pq.add(new Point(1, 2));
+        pq.add(new Point(1, 3));
+        pq.add(new Point(2, 3));
+        pq.add(new Point(2, 1));
+        System.out.println(pq);
+
+        while (!pq.isEmpty()) {
+            System.out.println(pq.poll() + " ");
+        }
+
+    }
+}
