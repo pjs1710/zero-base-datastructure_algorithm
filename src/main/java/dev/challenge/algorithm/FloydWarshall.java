357cf51effc41d4872009adb83c385c2f8a2ab5d
Park Jae Seong <timer973@naver.com>
2025-06-26 16:05:43 +0900

    Algorithm : Floyd-Warshall

---
+++ src/main/java/dev/challenge/algorithm/FloydWarshall.java
@@ -0,0 +1,56 @@
+package dev.challenge.algorithm;
+
+import java.io.*;
+import java.util.*;
+
+public class FloydWarshall {
+
+    static final int INF = 0XFFFFFFF;
+    static int N, M;
+    static List<List<Integer>> graph;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
+
+        graph = new ArrayList<>(Collections.nCopies(N + 1, null));
+        for (int i = 0; i <= N; i++) {
+            graph.set(i, new ArrayList<>(Collections.nCopies(N + 1, INF)));
+        }
+
+        for (int m = 1; m <= M; m++) {
+            st = new StringTokenizer(br.readLine());
+            int tu = Integer.parseInt(st.nextToken());
+            int tv = Integer.parseInt(st.nextToken());
+            int tw = Integer.parseInt(st.nextToken());
+            graph.get(tu).set(tv, Math.min(graph.get(tu).get(tv), tw));
+        }
+
+        for (int n = 1; n <= N; n++) {
+            graph.get(n).set(n, 0);
+        }
+
+        for (int m = 1; m <= N; m++) {
+            for (int s = 1; s <= N; s++) {
+                for (int e = 1; e <= N; e++) {
+                    var mn = Math.min(graph.get(s).get(s), graph.get(s).get(m) + graph.get(m).get(e));
+                    graph.get(s).set(e, mn);
+                }
+            }
+        }
+
+        for (int r = 1; r <= N; r++) {
+            for (int c = 1; c <= N; c++) {
+                if (graph.get(r).get(c) == INF) {
+                    System.out.print("0 ");
+                } else {
+                    System.out.print(graph.get(r).get(c) + "" );
+                }
+            }
+            System.out.println();
+        }
+    }
+}
